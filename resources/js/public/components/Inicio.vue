9bf707a21d8b2657431594c401ac85e1d66c5851
JesusAlmeda03 <jesus.almeda@ujed.mx>
2025-03-10 14:20:33 -0600

    test: components

--- resources/js/public/components/Inicio.vue
+++ resources/js/public/components/Inicio.vue
@@ -1,10 +1,10 @@
 <template>
-	<div>
-		<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat in fugiat, sunt minima ipsa nulla tempora quas, doloremque reprehenderit voluptatum numquam, illum dolorem quidem nobis voluptate error sequi ab! Nihil!</h1>
+	<div class="content">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat in fugiat, sunt minima ipsa nulla tempora quas, doloremque reprehenderit voluptatum numquam, illum dolorem quidem nobis voluptate error sequi ab! Nihil!
 	</div>
 </template>
 <script>
-export default{
-	name:'inicio',
-};
+	export default{
+		name:'inicio',
+	};
 </script>
