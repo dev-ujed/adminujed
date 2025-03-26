4024292d4f9c1fb38a3436b5e7143faef8443165
DESKTOP-D2GC4C5\yulis <yulissa.litzy@gmail.com>
2025-03-26 14:29:31 -0600

    inicio desarrollo de programas

--- administracion/urls.py
+++ administracion/urls.py
@@ -4,5 +4,6 @@ from  administracion.views import views, unidad, profesor
 urlpatterns = [
 	path('', views.home, name='home' ),
 	path('unidad/', unidad.index, name='unidad' ),
+	path('unidad/<int:id>/', unidad.detalles_unidad, name='detalles_unidad'),
 	path('profesor/', profesor.index, name='profesor' ),
 ]
