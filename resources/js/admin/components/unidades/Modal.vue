e3ae65998a372e5d3e7cb964e78184e5dd089c6e
DESKTOP-D2GC4C5\yulis <yulissa.litzy@gmail.com>
2025-04-07 14:30:25 -0600

    mostrar modal con la info de la materia

---
+++ resources/js/admin/components/unidades/Modal.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="modal modal-overlay" @click="handleClose">
+      <div class="base-modal__panel modal-groups__card">
+        <div class="modal-groups__close-button">
+            <button  @click="handleClose"><img src="/static/img/Icon x.png" alt="Icono cerrar"></button>
+        </div>
+        <div class="base-modal__heading" >
+            <h2 class="modal__title">{{ materia.desc_materia}}</h2>
+        </div>
+          <div class="base-modal__container">
+            <div class="base-modal__container-info">
+                <p><span>Clave: </span>{{ info.info_materia[0].clave }}</p>
+                <p><span>Horas: </span>{{ info.info_materia[0].horas }}</p>
+                <p><span>Créditos: </span>{{ info.info_materia[0].creditos }}</p>
+                <div v-if="info.info_materia[0].materias_encadenadas">
+                    <p class="mt-5"><img src="/static/img/Icon link.png" alt="Icono Link"><span>Materias encadenadas</span></p>
+                    <ul>
+                        <li>{{ info.info_materia[0].materias_encadenadas }}</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div class="base-modal__container-grupos">
+                <div class="grupos-select">
+                    <select class="select-field form-field">
+                        <option disabled selected>Semestre 2025-A</option>
+                    </select>
+                </div>
+                <p><span>Grupos asignados: </span>{{ gruposAsignados }} de {{ totalGrupos }}</p>
+                <div class="grupos">
+                    <div class="grupos__info" v-for="(grupo, index) in info.grupos" :key="index">
+                        <div class="grupos__info--header">
+                            <div><h1 class="grupos__title">Grupo {{ grupo.grupo }}</h1></div>
+                            <div class="grupos__cupo">
+                              <progress :id="'cupo-' + grupo.grupo" max="100" :class="getProgresoColor(grupo)" :value="(grupo.inscritos / grupo.cupo) * 100">{{ (grupo.inscritos / grupo.cupo * 100).toFixed(0) }}%</progress>
+                                <label for="cupo">{{ grupo.inscritos >= grupo.cupo ? 'Lleno' : 'Cupo' }}</label>
+                            </div>
+                        </div>
+                        <div class="grupos__info--body">
+                            <p><img src="/static/img/Icon students.png" alt="Icon students">{{ grupo.inscritos }} de {{ grupo.cupo }} estudiantes</p>
+                            <a href=""><img src="/static/img/Icon teacher.png" alt="Icon teacher">{{ grupo.nom_maestro }} <img class="icon-select" src="/static/img/Vectorb.png" alt="Selec"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </template>
+  
+<script>
+  export default {
+    name: 'Modal',
+  
+    props: {
+      materia: {
+        type: Object,
+        required: true
+      },
+
+      cve_carrera: {
+        type: [String, Number],
+        required: true
+      },
+
+      cve_plan: {
+        type: [String, Number],
+        required: true
+      },
+        
+      info: {
+        type: Object
+      }
+    },
+
+    computed: {
+        gruposAsignados() {
+            return this.info.grupos?.filter(g => g.nom_maestro !== 'Sin asignar').length || 0;
+        },
+        totalGrupos() {
+            return this.info.grupos?.length || 0;
+        }
+    },
+  
+    methods: {
+      getProgresoColor(grupo) {
+        const porcentaje = (grupo.inscritos / grupo.cupo) * 100;
+        if(porcentaje == 100){
+          console.log(porcentaje);
+          return 'progress-red';
+        }
+      },
+
+      handleClose() {
+        if (event.target.closest(".base-modal__panel")) return;
+        this.$emit('close');
+      }
+    }
+  };
+</script>
+
+  
--- resources/js/admin/components/unidades/Programa.vue
+++ resources/js/admin/components/unidades/Programa.vue
@@ -4,14 +4,13 @@
             <h1 class="db-heading--admin__title"><span>{{ carreraNombre }}</span></h1>
         </div>
         <div v-if="planes_actuales.length > 0">
-
             <div class="section">
                 <div class="container plan-container">
                     <div>
-                        <p>Selecciona un plan {{ planSeleccionado }}</p>
                         <select v-model="planSeleccionado" class="select-field form-field">
+                            <option disabled value="">-- Selecciona un plan --</option>
                             <option v-for="plan in planes_actuales" :key="plan.cve_plan" :value="plan.cve_plan">
-                                {{ plan.cve_plan }}
+                                {{ plan.desc_plan }}
                             </option>
                         </select>
                     </div>
@@ -25,7 +24,7 @@
                         <div v-for="(materiasPorSemestre, semestre) in materias" :key="semestre" class="semestre">
                             <div class="semestre-title">SEMESTRE {{ semestre }}</div>
                             <ul class="materias-list">
-                                <li v-for="materia in materiasPorSemestre" :key="materia.cve_materia">
+                                <li v-for="materia in materiasPorSemestre" :key="materia.cve_materia" @click="abrirModal(materia)">
                                     {{ materia.desc_materia }}
                                 </li>
                             </ul>
@@ -34,18 +33,26 @@
                 </div>
             </div>
 
+            <modal v-if="modalVisible" :materia="materiaSeleccionada" :cve_carrera="cve_carrera" :cve_plan="planSeleccionado" :info="materia_info" @close="cerrarModal" />
         </div>
         <div v-else class="section">
             <div class="container"><p>Sin planes activos</p></div>
         </div>
+
+
     </div>
 </template>
 
 <script>
     import axios from 'axios';
+    import Modal from './Modal.vue';
     
     export default{
         name: "programa",
+
+        components: {
+            Modal
+        },
 
         props: {
             cve_carrera: {
@@ -64,6 +71,9 @@
                 carreraNombre: '',
                 planSeleccionado: '',
                 materias: {},
+                materia_info: [],
+                modalVisible: false,
+                materiaSeleccionada: null,
             }
         },
 
@@ -71,8 +81,13 @@
             planSeleccionado(newPlan) {
                 if (newPlan) {
                     this.obtenerMaterias(newPlan);
+                    //this.materiaInfo(newPlan, this.cve_carrera);
                 }
             }
+        },
+
+        computed: {
+            
         },
 
         methods:{
@@ -81,17 +96,41 @@
                     .get(`/admi/materias_plan/?cve_plan=${cve_plan}`)
                     .then(response => {
                         this.materias = response.data;
-                        console.log(this.materias)
                     })
                     .catch(error => {
                         console.error("Error al obtener materias:", error);
                     });
+            },
+
+            materiaInfo(planSeleccionado, cve_carrera, cve_materia){
+
+                axios
+                    .get(`/admi/materia-info/`, { params: { plan: planSeleccionado, carrera: cve_carrera, cve_materia: cve_materia } })
+                    .then(response =>{
+                        this.materia_info = response.data;
+                        console.log(this.materia_info);
+                        this.modalVisible = true;
+                    })
+                    .catch(error => {
+                        console.error("Error al traer la info de la materia: ", error);
+                    })
+            },
+
+            abrirModal(materia) {
+                this.materiaSeleccionada = materia;
+                this.materiaInfo(this.planSeleccionado, this.cve_carrera, materia.cve_materia);
+            },
+
+                cerrarModal() {
+                this.modalVisible = false;
+                this.materiaSeleccionada = null;
             }
+            
         },
 
         mounted() {
-
             this.carreraNombre = localStorage.getItem('carrera_nombre') || 'Nombre no disponible';
+            
         }
     }
 </script>
